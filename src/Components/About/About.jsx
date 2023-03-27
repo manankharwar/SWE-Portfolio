faa371ca98570ae177194d0e29a8aeb7df49f954
Manan <kharwarm@mcmaster.ca>
2023-03-27 19:16:01 -0400

    About Me Section added

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import './About.css'
+import ME from './IMG_0443_square.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>
+      Get To Know
+      </h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt = "personal photo"/>
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+            <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+            <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ Clients</small>
+            </article>
+
+            <article className='about__card'>
+            <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>4+ Projects</small>
+            </article>
+          </div>
+
+          <p>
+            <strong>I am on a continuous journey to learn.</strong> <br /><br />
+
+            There is always something new to discover, perhaps a new kind of technology that can be applied in various ways or a new algorithm to reduce a coding challenge solution's time complexity. Nevertheless, these discoveries all serve one overarching goal: how can we, as humans, bring an impactful career to society and evolve success in every possible way.<br />
+            <br />
+            Systems are how we view the world. Design is how we change the world. <br /><br />
+            As a senior studying Engineering Physics (co-op) at McMaster University, I am excited to explore entrepreneurial ventures in the technology sector and incorporate innovative design into our everyday lives.
+          </p>
+
+          <a href="#contact" className = 'btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -3,17 +3,20 @@ import './Nav.css'
 import {AiOutlineHome, AiOutlineUser, AiOutlineAntDesign} from 'react-icons/ai'
 import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'     
 import {RiServiceLine} from 'react-icons/ri'
+import { useState } from 'react'
+
 
 // going to have about, experiences, projects, & contact
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a href='#'><AiOutlineHome /></a>
-      <a href='#about'><AiOutlineUser /></a>
-      <a href='#experience'><BiBook /></a>
-      <a href='#project'><RiServiceLine /></a>
-      <a href='#portfolio'><AiOutlineAntDesign /></a>
-      <a href='#contact'><BiMessageSquareDetail /></a>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} ><AiOutlineUser /></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href='#project' onClick={() => setActiveNav('#project')} className={activeNav === '#project' ? 'active' : ''}><RiServiceLine /></a>
+      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineAntDesign /></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
 
     </nav>
   )
