8b34a5c8f01fd74f19d8bbd21a802d47b8d8bec8
Manan Kharwar <44316521+manankharwar@users.noreply.github.com>
2023-03-27 21:13:40 -0400

    Update Experience.jsx

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Experience.css'
 
+// Interships at Bell, Intact, Ceridian, Pulse
 const Experience = () => {
   return (
     <section id='experience'>Experience</section>
