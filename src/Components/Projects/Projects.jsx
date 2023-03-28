998f2db60587fba7e22a21456f775d2a960b5f50
Manan Kharwar <44316521+manankharwar@users.noreply.github.com>
2023-03-27 21:13:02 -0400

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 import './Projects.css'
+
+// projects to add: 
+
+/*
+  . Decentralized Algo project
+  . Personal Website
+  . SHacks (Scotiabank)
+  . Hand Prosthesis
+  . McMaster Design League
+  . iRobot Roomba
+*/
 const Projects = () => {
   return (
     <section id='project'>Projects</section>
