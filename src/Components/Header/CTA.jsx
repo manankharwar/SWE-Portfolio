5d4c94cca54389ebce7e7a08f8e0e738a142fd43
Manan <kharwarm@mcmaster.ca>
2023-04-26 13:31:24 -0400

    updated hyperlink for resume

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className = 'cta'>
-        <a href = 'https://nurturingmachines.files.wordpress.com/2023/04/manan_kharwar_resume_2024_swe-1.pdf' target ="_blank" rel='noreferrer' download className='btn'>Download CV</a>
+        <a href = 'https://nurturingmachines.files.wordpress.com/2023/04/manan_kharwar_resume_2024.pdf' target ="_blank" rel='noreferrer' download className='btn'>Download CV</a>
         <a href = '#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
 
