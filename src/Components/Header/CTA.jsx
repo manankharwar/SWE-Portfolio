88dd4f9c48f30f991c559ae8f878176716bc2499
Manan <kharwarm@mcmaster.ca>
2023-04-28 19:00:05 -0400

    reverted my changes

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className = 'cta'>
-        <a href = 'Manan_Kharwar_Resume_2024.pdf' target ="_blank" rel='noreferrer' download className='btn'>Download CV</a>
+        <a href = 'https://nurturingmachines.files.wordpress.com/2023/04/manan_kharwar_resume_2024-2.pdf' target ="_blank" rel='noreferrer' download className='btn'>Download CV</a>
         <a href = '#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
 
